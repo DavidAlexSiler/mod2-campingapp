4ddfc0e949edd3c0d2d2816ff885f827ee9d645e
github username <github email address>
2019-07-09 18:09:03 -0400

    finished reservation functionality

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,42 @@
 class ReservationsController < ApplicationController
+
+    def index 
+        @reservations = Reservation.all 
+        
+    end
+
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        @reservation = Reservation.new(reservation_params)
+        @reservation.user_id = session[:user_id]
+        if @reservation.save
+            redirect_to reservation_path(@reservation)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @reservation = Reservation.find(params[:id])
+        if @reservation.user == current_user
+            @reservation.destroy
+        else
+            flash[:info] = "You are not allowed to delete other peoples reservations."
+        end
+        redirect_to reservations_path
+    end
+
+    private 
+
+    def reservation_params 
+        params.require(:reservation).permit(:user_id, :planet_id, :date_time, :cost)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate!
+    before_action :authenticate!, except: [:new]
 
 
     def index
